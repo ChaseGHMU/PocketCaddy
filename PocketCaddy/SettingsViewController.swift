abaf3e8236f5066691869c86b636a11603bfa6c4
Megan Cochran <megan.cochran@gmail.com>
2018-03-01 11:45:59 -0600

    Nav Bar/ Settings Page
    + Added Navigation Bar for Scores Tab to get to Settings Page
    + Hid Tab Bar while on Settings Page
    + Added View Controllers for Scores & Setting Pages

---
+++ PocketCaddy/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  PocketCaddy
+//
+//  Created by Megan Cochran on 3/1/18.
+//  Copyright © 2018 Chase Allen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       // self.tabBarController?.tabBar.isHidden = true // hide tab bar in this view
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
