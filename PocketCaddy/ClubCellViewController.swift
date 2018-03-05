c8ac617c4800c815a5d300cc4c2722fe3f5a952e
Megan Cochran <megan.cochran@gmail.com>
2018-03-04 20:36:15 -0600

    Swing Page
    + Added alert to allow user to add new swing distances to their club
    + Swing distances are then added to table view, however they do not
    persist when user leaves the view

--- PocketCaddy/ClubCellViewController.swift
+++ PocketCaddy/ClubCellViewController.swift
@@ -8,15 +8,54 @@
 
 import UIKit
 
-class ClubCellViewController: UIViewController {
+class ClubCellViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var clubName: UILabel!
     var name: String?
+    var distances: [Int] = []
+    
+    let cellReuseIdentifier = "cell"
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBAction func addSwing(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add New Swing", message: "How far did you hit the ball?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        // creating text field for club name
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter Distance"
+        }
+        // creates both add and cancel options for user
+        // alert.addAction(UIAlertAction(title: "Add", style: UIAlertActionStyle.default, handler: nil))
+        
+        let submitAction = UIAlertAction(title: "Add", style: .default, handler: { (action) -> Void in
+            // Get 1st TextField's text
+            let textField = alert.textFields![0]
+            print(textField.text!)
+            let textfieldInt: Int? = Int(textField.text!)
+            self.distances.append(textfieldInt!)
+            //print(self.distances)
+            self.tableView.reloadData() //reloads data so new club is displayed
+        })
+        alert.addAction(submitAction)
+        
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil))
+        
+        // show alert to user
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(name)
         clubName.text = name
+        
+        //tableview setup
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Do any additional setup after loading the view.
     }
@@ -24,6 +63,31 @@ class ClubCellViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //tableview functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.distances.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        //cell.textLabel?.text = self.distances[indexPath.row]
+        
+        var stringInt = String(self.distances[indexPath.row])
+        cell.textLabel?.text = stringInt
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+        print(distances[indexPath.row])
     }
     
 
