86496b7c024e3fea53dd1d567bfde23712ec9de6
Megan Cochran <megan.cochran@gmail.com>
2018-04-18 17:26:11 -0500

    Swings Load Again

--- PocketCaddy/Practice Tab/ClubCellViewController.swift
+++ PocketCaddy/Practice Tab/ClubCellViewController.swift
@@ -158,21 +158,21 @@ class ClubCellViewController: UIViewController, UITableViewDelegate, UITableView
         // create a new cell if needed or reuse an old one
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
         
-        if let cell = cell as? PracticeClubViewCell{
-            if(indexPath.row % 2 == 0)
-            {
-                cell.backgroundColor = UIColor(red:1.00, green:0.98, blue:0.96, alpha:1.0)
-            }
-                
-            else
-            {
-                cell.backgroundColor = UIColor(red:1.00, green:0.98, blue:0.93, alpha:1.0)
-            }
+//        if let cell = cell as? PracticeClubViewCell{
+//            if(indexPath.row % 2 == 0)
+//            {
+//                cell.backgroundColor = UIColor(red:1.00, green:0.98, blue:0.96, alpha:1.0)
+//            }
+//                
+//            else
+//            {
+//                cell.backgroundColor = UIColor(red:1.00, green:0.98, blue:0.93, alpha:1.0)
+//            }
             // set the text from the data model
             let stringInt = String(self.swings[indexPath.row].distance)
             cell.textLabel?.text = stringInt + " yards"
             print(stringInt)
-        }
+        //}
         return cell
     }
     
@@ -196,7 +196,7 @@ class ClubCellViewController: UIViewController, UITableViewDelegate, UITableView
             let yesAction = UIAlertAction(title: "Yes", style: .default, handler: { (action) -> Void in
                 self.swings.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .fade)
-                PocketCaddyData.delete(table: .swings, id: self.swings[indexPath.row].id) //deletes swings from db
+               // PocketCaddyData.delete(table: .swings, id: self.swings[indexPath.row].id) //deletes swings from db
                 self.tableView.reloadData()
             })
             deleteAlert.addAction(yesAction)
