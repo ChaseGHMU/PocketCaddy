9bc9433ee3e0e37b0a0a389d08606c9175e0ab08
Megan Cochran <megan.cochran@gmail.com>
2018-04-18 22:25:34 -0500

    Score View
    + Score view now loads the course name and date of game

--- PocketCaddy/Score Tab/DetailedGameViewController.swift
+++ PocketCaddy/Score Tab/DetailedGameViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class DetailedGameViewController: UIViewController {
     
-    var club:Clubs?
-    var swings: [Swings] = []
+   
     
     var game:Games?
     var scores: [Scores] = []
-
+    @IBOutlet weak var courseName: UILabel!
+    @IBOutlet weak var gameDate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        courseName.text = getName(courseId: (game?.courseId)!)
+        gameDate.text = game?.gameTime
 
         // Do any additional setup after loading the view.
     }
@@ -27,6 +30,17 @@ class DetailedGameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func getName(courseId: String) -> String {
+        if courseId == "1"{
+            return "Triple Lakes Golf Club"
+        }
+        if courseId == "2" {
+            return "L.A. Nickell"
+        }
+        else {
+            return "Golf Course"
+        }
+    }
 
     /*
     // MARK: - Navigation
