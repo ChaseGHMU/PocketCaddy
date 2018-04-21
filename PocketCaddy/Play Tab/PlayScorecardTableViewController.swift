5e821a413e337ef8e21abdf1527e1f92819c8986
Chase Allen <challenj23@gmail.com>
2018-04-21 14:34:44 -0500

    button added to bottom of play's scorecard

--- PocketCaddy/Play Tab/PlayScorecardTableViewController.swift
+++ PocketCaddy/Play Tab/PlayScorecardTableViewController.swift
@@ -101,6 +101,9 @@ class PlayScorecardTableViewController: UITableViewController {
         return cell
     }
 
+    @IBAction func finishRound(_ sender: Any) {
+        print("button clicked")
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
