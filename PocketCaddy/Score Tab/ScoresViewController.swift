7e2522317990e63b6973802b8ca1d63a23d46f26
Megan Cochran <megan.cochran@gmail.com>
2018-04-21 18:47:22 -0500

    Fixed Logout Button

--- PocketCaddy/Score Tab/ScoresViewController.swift
+++ PocketCaddy/Score Tab/ScoresViewController.swift
@@ -139,22 +139,21 @@ class ScoresViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
 
-    @IBAction func logout(_ sender: Any) {
-        defaults.set(nil, forKey: "id")
-        defaults.set(nil, forKey: "username")
-        defaults.set(nil, forKey: "userId")
-        defaults.set(nil, forKey: "created")
-        defaults.set(false, forKey: "isLoggedIn")
-        self.performSegue(withIdentifier: "logoutSegue",sender: self)
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         if let destination = segue.destination as? DetailedGameViewController, let index = scoresTableView.indexPathForSelectedRow {
            // destination.club = clubs[index.row]
             destination.game = games[index.row]
-            
+        }
+        //logoutSegue
+        if let destination = segue.destination as? LoginViewController {
+            defaults.set(nil, forKey: "id")
+            defaults.set(nil, forKey: "username")
+            defaults.set(nil, forKey: "userId")
+            defaults.set(nil, forKey: "created")
+            defaults.set(false, forKey: "isLoggedIn")
+            print("Logging out")
         }
     }
 }
